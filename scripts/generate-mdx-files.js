93cb36a0227e6e1db4e0c90d1af0d74dca263589
Frankie Bagnardi <f.bagnardi@gmail.com>
2019-12-25 15:27:41 -0500

    build: setup docz and script to generate condtion .mdx files

---
+++ scripts/generate-mdx-files.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const glob = require('glob');
+
+{
+  const files = glob.sync('src/cond/*.js').map((js) => ({
+    js,
+    mdx: js.replace(/\.js$/, '.mdx'),
+    name: js.replace(/.*cond\/(\w+)\.js$/, '$1'),
+  }));
+
+  const tpl = fs.readFileSync('scripts/internal/cond-mdx-template.txt', 'utf-8');
+
+  for (const { js, mdx, name } of files) {
+    if (!fs.existsSync(mdx)) {
+      console.log(`Generating .mdx for ${name} (${mdx})`);
+      const contents = tpl.replace(/NAME/g, name);
+      console.log(contents);
+      fs.writeFileSync(mdx, contents);
+    }
+  }
+}
