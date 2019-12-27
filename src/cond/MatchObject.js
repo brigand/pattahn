182e66a433267e4ae61189d283b5d2ea39e5f1a3
Frankie Bagnardi <f.bagnardi@gmail.com>
2019-12-27 16:30:27 -0500

    feat: impl condition MatchObject

---
+++ src/cond/MatchObject.js
@@ -0,0 +1,58 @@
+const { Match, Condition } = require('../core');
+
+class Output extends Condition {
+  constructor(condition = null) {
+    this.condition = condition;
+  }
+  impl(...args) {
+    if (this.condition) {
+      return this.condition.impl(...args);
+    } else {
+      return new Match(...args);
+    }
+  }
+}
+
+class MatchObject extends Condition {
+  /**
+   * Simple identity condition.
+   */
+  constructor(pattern) {
+    super('MatchObject');
+
+    this.pairs = [];
+    for (const key of Object.keys(pattern)) {
+      const condition = intoCondition(pattern[key]);
+      this.pairs.push([key, condition]);
+    }
+  }
+
+  impl(object, ...args) {
+    if (!object || typeof object !== 'object') {
+      return null;
+    }
+
+    const outputs = [object];
+    for (const [key, condition] of this.pairs) {
+      const match = condition.exec(object[key], ...args);
+      if (match) {
+        if (condition instanceof Output) {
+          outputs.push(match[0]);
+        }
+      } else {
+        return null;
+      }
+    }
+
+    return new Match(...outputs);
+  }
+
+  clone() {
+    return new MatchObject();
+  }
+}
+
+module.exports = MatchObject.factory;
+module.exports.Output = Output.factory;
+
+const intoCondition = require('../intoCondition');
